797c557a08c4b9205927464d0126d69e973c2da6
Marina-str-vit <meribsv@gmail.comy>
2024-11-22 19:55:21 +0200

    add files

--- js/10_mod/10_03-notification.js
+++ js/10_mod/10_03-notification.js
@@ -3,30 +3,42 @@
  * - Ховаємо через певний час
  * - Ховаємо при кліці
  * - Не забуваємо чистити таймер
+ *
+ * наприклад, так працює баннер з рекламою
+ *
  */
 
 const NOTIFICATION_DELAY = 3000;
+// отримали віконце, що буду ховати та показувати
 const notification = document.querySelector(".js-alert");
+
+//let = щоб можна було її переініціалізовувати та використовувати будь-якій локальній області видимості, можна просто без null
 let timerId = null;
 
+//при кліку ховаємо вікно
 notification.addEventListener("click", onNotificationClick);
 
 showNotification();
 
-function showNotification() {
-  notification.classList.add("is-visible");
+// додали клас, щоб зробити ел-нт видимим та потім через 3сек. воно закриється
 
+function showNotification() {
+  notification.classList.add("is-visible"); //додали клас
+  // звернулися до ел-ту і закрили його через NOTIFICATION_DELAY
   timerId = setTimeout(() => {
+    // timerId --> це ідентифікатор функції
     console.log("hide");
     hideNotification();
   }, NOTIFICATION_DELAY);
 }
 
-function onNotificationClick() {
-  hideNotification();
-  clearTimeout(timerId);
-}
-
+//створили окрему функцію де видялаяємо клас видимості!!!
+// цю функцію потім підставляємо в  таймер setTimeout її видимості
 function hideNotification() {
   notification.classList.remove("is-visible");
+}
+
+function onNotificationClick() {
+  hideNotification();
+  clearTimeout(timerId); // timerId --> це ідентифікатор функції
 }
---
+++ js/10_mod/10_05-timer.js
@@ -0,0 +1,167 @@
+/**
+ *
+ */
+//через new створюємо екземпляр классу, тепер  currentDate - це об'єкт
+// const currentDate = new Date();
+// console.log(currentDate); // об'єкт приведений до рядка і показує реальний повний час
+
+// console.log(currentDate.getTime()); //отримую Date у вигляді числа, щоб потім можна розраховувати різничю в часі
+// // якщо потрібно у мілісекундах
+// console.log(Date.now()); // - це статичний метод
+
+// console.log(currentDate.getDay()); //отримуємо день попорядку де Неділя == 0, Понеділок == 1 ....
+// console.log(currentDate.getMonth()); // Січень == 0, Лютий == 1 .....
+
+// const d1 = Date.now();
+// setTimeout(() => {
+//   const d2 = Date.now();
+//   console.log(d2 - d1);
+// }, 2000); // є погрешность із-за навантаження на процессор
+
+/**==============================================================
+ *
+ * Напишемо клас Timer, який буде
+ * запускати! та зупиняти!! відлік часу
+ *
+ */
+//т.як класи у кнопках однакові, то звертаємось до data-атрибутів
+const startBtn = document.querySelector("button[data-action-start]");
+const stopBtn = document.querySelector("button[data-action-stop]");
+const clockface = document.querySelector(".clockface");
+/**
+ *  створили конструктор де ми створили властивість,
+ * яка буде перевіряти, чи запущений чи ні таймер
+ */
+class Timer {
+  constructor({ onTick }) {
+    this.isActive = false;
+    this.onTick = onTick;
+    this.intervalId = null;
+
+    this.init();
+  }
+
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+  start() {
+    if (this.isActive) {
+      // у даному випадку це булеве значення, а для if по дефолту буль буде true,
+      //тому коли туди попадає значення false (строка 37), то функція одразу припиниться
+      // і піде далі виконуватися
+      return;
+    }
+    // тут знайде нове значення і почне виконуватися
+    this.isActive = true;
+
+    const startTime = Date.now(); // отримали час коли клікнули по кнопці startBtn
+    // отримаємо значення скільки часу пройшло і він актуалізовується кожну секунду
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      // через this викликається функція, що є не в локальній видимості
+      const time = this.getTimeComponents(deltaTime);
+
+      this.onTick(time);
+    }, 1000);
+  }
+  //
+  //
+  //
+  //ця конструкція вже є в бібліотеках, а це її розбір: що з а чим і як
+  getTimeComponents(time) {
+    //відкидую добу
+    const hours = this.pad(
+      //             відкидуємо сек   хв  години         сек   хв
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) //Math.floor - округлілі
+    );
+    //відкидуємо години                              сек  хв      тут секунди в хвилини
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    //відкидуємо хвилини, залишаємо лише секунди
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    //    короткий  синтаксис
+    return { hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+//
+const timer = new Timer({
+  onTick: updateClockfase,
+});
+//
+//коли передаємо колбек функцію, то у нас втрачений контекст,
+//тоді її підв'язати методом bind(), яки передає посилання
+startBtn.addEventListener("click", timer.start.bind(timer));
+stopBtn.addEventListener("click", timer.stop.bind(timer));
+
+function updateClockfase({ hours, mins, secs }) {
+  clockface.textContent = `${hours}:${mins}:${secs}`;
+}
+
+let isActive = false;
+let intervalId = null;
+
+init();
+
+function start() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+  const startTime = Date.now();
+
+  intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = currentTime - startTime;
+    const time = getTimeComponents(deltaTime);
+
+    onTick(time);
+  }, 1000);
+}
+
+function getTimeComponents(time) {
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { hours, mins, secs };
+}
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+function stop() {
+  clearInterval(intervalId);
+  isActive = false;
+  const time = getTimeComponents(0);
+  onTick(time);
+}
+
+function init() {
+  const time = getTimeComponents(0);
+  onTick(time);
+}
+
+function onTick({ hours, mins, secs }) {
+  clockface.textContent = `${hours}:${mins}:${secs}`;
+}
+
+startBtn.addEventListener("click", start);
+stopBtn.addEventListener("click", stop);
